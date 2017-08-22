febdd2e9a3ebe018cb15e2dbc34ad8df844f9894
benoitme <lelabodebiologie@gmail.com>
2017-08-22 17:06:36 +0200

    Random Forest Classifier

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import math
 
 # Data import
@@ -10,32 +11,36 @@ raw_test = pd.read_csv("data/test.csv")
 
 # Transform data in something more "readable" for ML algorithms
 train = pd.DataFrame()
-ytrain = pd.DataFrame()
 train["PassengerId"] = raw_data["PassengerId"]
 train["Pclass"] = raw_data["Pclass"]
 train["NameLen"] = raw_data["Name"].apply(len)
 train["Sex"] = raw_data["Sex"].apply(lambda x: 0 if x =="female" else 1)
 train["Age"] = raw_data["Age"].apply(lambda x: 0 if math.isnan(x) else int(x))
 train["Cabin"] = raw_data["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
-ytrain["Survived"] = raw_data["Survived"]
+train["SibSp"] = raw_data["SibSp"]
+train["Parch"] = raw_data["Parch"]
+train["Survived"] = raw_data["Survived"]
 
 # Same thing on test sample
 test = pd.DataFrame()
-ytest = pd.DataFrame()
 test["PassengerId"] = raw_test["PassengerId"]
 test["Pclass"] = raw_test["Pclass"]
 test["NameLen"] = raw_test["Name"].apply(len)
 test["Sex"] = raw_test["Sex"].apply(lambda x: 0 if x =="female" else 1)
 test["Age"] = raw_test["Age"].apply(lambda x: 0 if math.isnan(x) else int(x))
 test["Cabin"] = raw_test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+test["SibSp"] = raw_data["SibSp"]
+test["Parch"] = raw_data["Parch"]
 
 # Première tentative en knn neighbors
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(train, ytrain)
+X_train = train[["Pclass", "NameLen", "Sex", "Age", "Cabin", "SibSp", "Parch"]]
+y_train = np.ravel(train["Survived"])
+X_test = test[["Pclass", "NameLen", "Sex", "Age", "Cabin", "SibSp", "Parch"]]
 
-final_test = pd.DataFrame({"PassengerId":test["PassengerId"], "Survived":knn.predict(test)})
-final_test.to_csv("Submission.csv")
-print(knn.predict(test))
+rfc = RandomForestClassifier(n_estimators=1000)
+rfc.fit(X_train, y_train)
+
+final_test = pd.DataFrame({"PassengerId":test["PassengerId"], "Survived":rfc.predict(X_test)})
+final_test.to_csv("Submission.csv", index=False)
