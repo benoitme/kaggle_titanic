097a375c9ef4825a29b4c74bb5c3a2a3d44ee19b
benoitme <lelabodebiologie@gmail.com>
2017-08-22 15:42:13 +0200

    First test

--- analysis.py
+++ analysis.py
@@ -2,3 +2,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+# Data import
+raw_data = pd.read_csv("data/train.csv")
+raw_test = pd.read_csv("data/test.csv")
+
+# Transform data in something more "readable" for ML algorithms
+train = pd.DataFrame()
+ytrain = pd.DataFrame()
+train["PassengerId"] = raw_data["PassengerId"]
+train["Pclass"] = raw_data["Pclass"]
+train["NameLen"] = raw_data["Name"].apply(len)
+train["Sex"] = raw_data["Sex"].apply(lambda x: 0 if x =="female" else 1)
+train["Age"] = raw_data["Age"].apply(lambda x: 0 if math.isnan(x) else int(x))
+train["Cabin"] = raw_data["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+ytrain["Survived"] = raw_data["Survived"]
+
+# Same thing on test sample
+test = pd.DataFrame()
+ytest = pd.DataFrame()
+test["PassengerId"] = raw_test["PassengerId"]
+test["Pclass"] = raw_test["Pclass"]
+test["NameLen"] = raw_test["Name"].apply(len)
+test["Sex"] = raw_test["Sex"].apply(lambda x: 0 if x =="female" else 1)
+test["Age"] = raw_test["Age"].apply(lambda x: 0 if math.isnan(x) else int(x))
+test["Cabin"] = raw_test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+
+# Première tentative en knn neighbors
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(train, ytrain)
+
+final_test = pd.DataFrame({"PassengerId":test["PassengerId"], "Survived":knn.predict(test)})
+final_test.to_csv("Submission.csv")
+print(knn.predict(test))
--- data_exploration.py
+++ data_exploration.py
@@ -2,6 +2,40 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import math
 
 # Data import
-raw_data = pd.read_csv("data/train.csv")
+raw_data = pd.read_csv('data/train.csv')
+
+# Let's start by looking at the data
+print(raw_data.head())
+print('---------------------------------------------')
+print(raw_data.info())
+print('---------------------------------------------')
+print(raw_data.describe())
+print('---------------------------------------------')
+
+# General analysis : number of Nan in the data
+print(raw_data.isnull().sum())
+
+# Visual data analysis
+# Let's display plots of the different characteristics
+# Transform data in something more 'readable' for ML algorithms
+data = pd.DataFrame()
+data['PassengerId'] = raw_data['PassengerId']
+data['Survived'] = raw_data['Survived']
+data['Pclass'] = raw_data['Pclass']
+data['NameLen'] = raw_data['Name'].apply(len)
+data['Sex'] = raw_data['Sex'].apply(lambda x: 0 if x =='female' else 1)
+data['Age'] = raw_data['Age'].apply(lambda x: 0 if math.isnan(x) else int(x))
+data['Cabin'] = raw_data['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
+data['Embarked'] = raw_data['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+# Pairplot
+colormap = plt.cm.viridis
+plt.figure(figsize=(12,12))
+plt.title('Pearson Correlation of Features', y=1.05, size=15)
+sns.heatmap(data.astype(float).corr(),linewidths=0.1,vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
+plt.show()
+
