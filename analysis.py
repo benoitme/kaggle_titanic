8fb23ce9d2ed58b96365b45d476015dc1386cedd
benoitme <lelabodebiologie@gmail.com>
2017-08-30 16:30:53 +0200

    New Features

--- analysis.py
+++ analysis.py
@@ -11,23 +11,15 @@ raw_test = pd.read_csv('data/test.csv')
 
 # Transform data in something more 'readable' for ML algorithms
 train = pd.DataFrame()
-train['PassengerId'] = raw_data['PassengerId']
-train['Pclass'] = raw_data['Pclass']
-train['NameLen'] = raw_data['Name'].apply(len)
-train['Sex'] = raw_data['Sex'].apply(lambda x: 0 if x =='female' else 1)
-train['Age'] = raw_data['Age'].apply(lambda x: 0 if math.isnan(x) else int(x))
-train['Cabin'] = raw_data['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
-train['SibSp'] = raw_data['SibSp']
-train['Parch'] = raw_data['Parch']
-raw_data['Fare'] = raw_data['Fare'].fillna(value=raw_data['Fare'].median())
-train['Fare'] = raw_data['Fare'].round().apply(int)
-raw_data['Embarked'] = raw_data['Embarked'].fillna(value='S')
-train['Embarked'] = raw_data['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-train['Family'] = 0
-for index, row in raw_data.iterrows():
-    train['Family'][index] = row['SibSp'] + row['Parch'] + 1
+train['PassengerId'] = raw_train['PassengerId']
+train['Survived'] = raw_train['Survived']
+train['Pclass'] = raw_train['Pclass']
+train['Sex'] = raw_train['Sex'].apply(lambda x: 0 if x =='female' else 1)
+train['NameLen'] = raw_train['Name'].apply(len)
+train['Child'] = raw_train['Age'].apply(lambda x:1 if x<18 else 0)
+train['HasCabin'] = raw_train["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 train['Title'] = 0
-for index, row in raw_data.iterrows():
+for index, row in raw_train.iterrows():
     if 'Mr' in row['Name']:
         train['Title'][index] = 1
     elif 'Miss' in row['Name']:
@@ -38,27 +30,30 @@ for index, row in raw_data.iterrows():
         train['Title'][index] = 4
     else:
         train['Title'][index] = 5
-train['Survived'] = raw_data['Survived']
+train['Family'] = 0
+train['Family'] = raw_train['SibSp'] + raw_train['Parch'] + 1
+train['SibSp'] = raw_data['SibSp']
+train['Parch'] = raw_data['Parch']
+train['HasFam'] = 0
+train['HasFam'][train['Family']>1] = 1
+raw_train['Embarked'] = raw_train['Embarked'].fillna(value='S')
+train['Embarked'] = raw_train['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+train['Fare'] = raw_train['Fare']
+train['CatFare'] = 0
+train['CatFare'][train['Fare'] <= 7.91] = 0
+train['CatFare'][(train['Fare'] > 7.91) & (train['Fare'] <= 14.454)] = 1
+train['CatFare'][(train['Fare'] > 14.454) & (train['Fare'] <= 31)]= 2
+train['CatFare'][ train['Fare'] > 31] = 3
 
-
-# Same thing on test sample
+# Same for test
 test = pd.DataFrame()
 test['PassengerId'] = raw_test['PassengerId']
 test['Pclass'] = raw_test['Pclass']
-test['NameLen'] = raw_test['Name'].apply(len)
 test['Sex'] = raw_test['Sex'].apply(lambda x: 0 if x =='female' else 1)
-test['Age'] = raw_test['Age'].apply(lambda x: 0 if math.isnan(x) else int(x))
-test['Cabin'] = raw_test['Cabin'].apply(lambda x: 0 if type(x) == float else 1)
-test['SibSp'] = raw_test['SibSp']
-test['Parch'] = raw_test['Parch']
-raw_test['Fare'] = raw_test['Fare'].fillna(value=raw_test['Fare'].median())
-test['Fare'] = raw_test['Fare'].round().apply(int)
-raw_test['Embarked'] = raw_test['Embarked'].fillna(value='S')
-test['Embarked'] = raw_test['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+test['NameLen'] = raw_test['Name'].apply(len)
+test['Child'] = raw_test['Age'].apply(lambda x:1 if x<18 else 0)
+test['HasCabin'] = raw_test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 test['Title'] = 0
-test['Family'] = 0
-for index, row in raw_test.iterrows():
-    test['Family'][index] = row['SibSp'] + row['Parch'] + 1
 for index, row in raw_test.iterrows():
     if 'Mr' in row['Name']:
         test['Title'][index] = 1
@@ -69,16 +64,30 @@ for index, row in raw_test.iterrows():
     elif 'Master' in row['Name']:
         test['Title'][index] = 4
     else:
-        test['Title'][index] = 5
+        train['Title'][index] = 5
+test['Family'] = 0
+test['Family'] = raw_test['SibSp'] + raw_test['Parch'] + 1
+test['SibSp'] = raw_test['SibSp']
+test['Parch'] = raw_test['Parch']
+test['HasFam'] = 0
+test['HasFam'][train['Family']>1] = 1
+raw_train['Embarked'] = raw_test['Embarked'].fillna(value='S')
+test['Embarked'] = raw_test['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+test['Fare'] = raw_test['Fare']
+test['CatFare'] = 0
+test['CatFare'][test['Fare'] <= 7.91] = 0
+test['CatFare'][(test['Fare'] > 7.91) & (test['Fare'] <= 14.454)] = 1
+test['CatFare'][(test['Fare'] > 14.454) & (test['Fare'] <= 31)]= 2
+test['CatFare'][test['Fare'] > 31] = 3
 
 
 # Lets combine the results of all the classifiers models
 from sklearn.ensemble import RandomForestClassifier
 
 # Select the most importants features
-X_train = train[['Pclass', 'NameLen', 'Sex', 'Age', 'Cabin', 'Embarked', 'Family', 'Title']]
+X_train = train[['PPclass', 'Sex', 'Child', 'HasCabin', 'Embarked',  'Title', 'HasFam', 'CatFare', 'Family']]
 y_train = np.ravel(train['Survived']) 
-X_test = test[['Pclass', 'NameLen', 'Sex', 'Age', 'Cabin', 'Embarked', 'Family', 'Title']]
+X_test = test[['PPclass', 'Sex', 'Child', 'HasCabin', 'Embarked',  'Title', 'HasFam', 'CatFare', 'Family']]
 
 # Fit the model
 rfc = RandomForestClassifier(max_features='sqrt', n_estimators=1000, min_samples_leaf=3)
